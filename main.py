6758e6a0a735ca6fde140aeb25cdf51a50c29d4e
bittib010 <77796831+bittib010@users.noreply.github.com>
2021-02-02 14:30:31 +0100

    Some fixes
    Not all. Still needs to sort out correct output - Error gets printed, but may have been sorted out by the above exception handling. Looking into it

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import os, sys
+import os
 from stat import *
 
-root_dir = 'C:\\Users\\Skaft\\Desktop\\'
+# Access denied was a huge problem. This link solved it for me:
+#   https://stackoverflow.com/questions/54042613/python-getting-errors-reading-directory-in-windows/54046850
+# Learning about hard links and junctions. This is one of the disadvantages of the output,
+# not showing hardlinks and junctions
 
-
-print(os.listdir(root_dir))
+root_dir = 'C:\\'
 
 
 def walktree(top):
@@ -13,14 +15,19 @@ def walktree(top):
         mode = os.stat(pathname)[ST_MODE]
         if S_ISDIR(mode):
             print('<details class="tab"><summary><img class="icon" src="folder.jpg">{}/ </summary>'.format(f))
-            walktree(pathname)
+            try:
+                walktree(pathname)
+            except Exception as e:
+                #print('!ERROR: {}, {}'.format(e, pathname))
+                pass
             print('</details>')
+
         elif S_ISREG(mode):
             print('\t<span class="tab"> <img class="icon" src="file.jpg">{} </span><br>'.format(f))
         else:
             # Unknown file type, print a message
-            print('************************************** UNKNOWN FILETYPE %s' % pathname)
+            print('************************************** UNKNOWN FILETYPE %s' % pathname)  #TODO: Check if this is outputted
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     walktree(root_dir)
