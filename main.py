bec5785dff5ec1ca73ac7de2beeb6227373d8c1f
bittib010 <adriankydlandskaftun@Adrians-MBP.home>
2021-03-18 23:58:56 +0100

    Made searches available
    Should add background color change to elements found. Anyone happy to help with this kinda Javascript?

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from stat import *
 # Learning about hard links and junctions. This is one of the disadvantages of the output,
 # not showing hardlinks and junctions
 
-root_dir = 'C:\\'
+root_dir = '/Users/adriankydlandskaftun/Documents/'
+
 
 
 def walktree(top):
@@ -14,7 +15,7 @@ def walktree(top):
         pathname = os.path.join(top, f)
         mode = os.stat(pathname)[ST_MODE]
         if S_ISDIR(mode):
-            print('<details class="tab"><summary><img class="icon" src="folder.jpg">{}/ </summary>'.format(f))
+            print('\t<details class="tab"><summary><img class="icon" src="folder.jpg">{}/ </summary>'.format(f))
             try:
                 walktree(pathname)
             except Exception as e:
@@ -23,11 +24,41 @@ def walktree(top):
             print('</details>')
 
         elif S_ISREG(mode):
-            print('\t<span class="tab"> <img class="icon" src="file.jpg">{} </span><br>'.format(f))
+            # The <p hidden> is for containing tags for searching
+            # TODO: Make more visible by adding linebreaks for tooltips (to come), filenames and tags?????
+            print('\t\t<span class="tab myTags"> <img class="icon" src="file.jpg">{} <p hidden> </p> </span><br>'.format(f))
         else:
             # Unknown file type, print a message
             print('************************************** UNKNOWN FILETYPE %s' % pathname)  #TODO: Check if this is outputted
 
 
 if __name__ == "__main__":
+    print('''
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Forensic pocket reference for file structure view</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+        <form id="searchForm" action="javascript:search();">
+            <div class="input-group">
+                <input type="text" id="searchItem" class="form-control" placeholder="Enter filename or any text/tag">
+                <button id="go" type="button" onclick="document.getElementById('searchForm').submit(); return false;"> </button>
+
+            </div>
+        </form>
+        
+
+        ''')
+    
+    print('<details class="tab"><summary><img class="icon" src="folder.jpg">{}/ </summary>'.format(root_dir))
     walktree(root_dir)
+    print('''
+            </details>
+            <script src="javascript.js"></script>
+    </body>
+</html>''')
